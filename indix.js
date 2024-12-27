0869c8f3a9686a83603ec0d8293e3f8e2480e61e
mouhamedbouzayan <bouzimohammed177@gmail.com>
2024-12-27 17:38:42 +0100

    BREIF 3 application de gestion des tache

---
+++ indix.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./routers/route'); 
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api', route);
+const mongoDBURL = 'mongodb://127.0.0.1:27017/BRIEF3';
+// Connect to MongoDB
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err.message);
+    }); 
+app.listen(8000, () => {
+    console.log('Server is running on port 5000');
+});
